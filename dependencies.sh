854a515fec1a9376d57ba03f3fb52751c961e5b6
downingbots <downingbots@gmail.com>
2022-02-03 22:23:08 -0800

    Add files via upload

---
+++ dependencies.sh
@@ -0,0 +1,11 @@
+sudo apt-get install python4-opencv
+pip3 install imutils
+pip3 install matplotlib
+pip3 install scikit-image
+pip3 install shapely
+pip3 install pandas
+pip3 install tqdm
+pip3 install sklearn
+pip3 install opencv-contrib-python
+
+
